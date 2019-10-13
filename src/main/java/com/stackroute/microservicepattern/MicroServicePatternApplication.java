0683e79a660e1c44eca7786f740b7c8d3f108676
vinod.kalamati <vinod.kalamati@cgi.com>
2019-10-13 18:45:15 +0530

    module

---
+++ src/main/java/com/stackroute/microservicepattern/MicroServicePatternApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.microservicepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicePatternApplication.class, args);
+	}
+
+}
